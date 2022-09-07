1de82abd3d9a1c5f3b7f507008043f1bfc4e783b
Wahyu Pratama <wahyulucky28@gmail.com>
2022-09-07 22:09:51 +0700

    testing api

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 const searchBtn = document.querySelector("#search-btn");
+const mealList = document.querySelector("#meal");
 
 searchBtn.addEventListener("click", searchMeal);
 
 function searchMeal() {
     const searchInputText = document.querySelector("#search-input").value.trim();
-    fetch(`www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputText}`)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => {
+            let html = "";
+            if (data.meals) {
+                console.log(data.meals);
+                data.meals.forEach(meal => {
+                    html += `
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <img src="${meal.strMealThumb}"
+                                class="card-img-top" alt="testing-image">
+                            <div class="card-body text-center">
+                                <h5 class="card-title">${meal.strMeal}</h5>
+                                <a href="#" class="btn btn-success">Get Recipe</a>
+                            </div>
+                        </div>
+                    </div>
+                    `;
+                    mealList.innerHTML = html;
+                })
+            } else {
+                alert('Meal not found');
+            }
+        });
 }
